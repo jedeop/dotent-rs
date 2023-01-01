4e1f583b9f421b1c9884bdb07b9c960b8ec4007e
jedeop <me@jedeop.dev>
2023-01-01 23:57:42 +0900

    ✨ feat: add Id::as_str

--- src/project/common.rs
+++ src/project/common.rs
@@ -2,3 +2,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Id(String);
+
+impl Id {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
